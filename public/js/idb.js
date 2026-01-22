ceccbeeb5bee0b4b8ac19e7690691ffdc0140f6c
Shane Wade <shanebramble96@hotmail.com>
2026-01-21 20:07:05 -0600

    Establish indexedDB connection

---
+++ public/js/idb.js
@@ -0,0 +1,28 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database called pwa_budget_tracker and set it to version 1
+const request = indexedDB.open("pwa_budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+  // save a reference to the database
+  const db = event.target.result;
+  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function(event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
+
+  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  if (navigator.onLine) {
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    // uploadTransaction();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
